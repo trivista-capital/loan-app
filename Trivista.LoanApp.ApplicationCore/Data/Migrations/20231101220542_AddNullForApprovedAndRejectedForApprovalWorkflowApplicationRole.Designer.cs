1d5c4fa2df0b20a759370065f123e086f95adbf4
Babafemi <femi.ibitolu@gmail.com>
2023-11-02 06:46:41 +0100

    Added migration for rejected and approved fields

--- Trivista.LoanApp.ApplicationCore/Data/Migrations/20231101220542_AddNullForApprovedAndRejectedForApprovalWorkflowApplicationRole.Designer.cs
+++ Trivista.LoanApp.ApplicationCore/Data/Migrations/20231101220542_AddNullForApprovedAndRejectedForApprovalWorkflowApplicationRole.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trivista.LoanApp.ApplicationCore.Data.Context;
 
@@ -11,9 +12,11 @@ using Trivista.LoanApp.ApplicationCore.Data.Context;
 namespace Trivista.LoanApp.ApplicationCore.Data.Migrations
 {
     [DbContext(typeof(TrivistaDbContext))]
-    partial class TrivistaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231101220542_AddNullForApprovedAndRejectedForApprovalWorkflowApplicationRole")]
+    partial class AddNullForApprovedAndRejectedForApprovalWorkflowApplicationRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
